96e2da0f61921c25fe91bf04cce4a845ce7c9e33
Baozoulolw <Baozoulolw@Gmail.com>
2021-12-10 00:01:46 +0800

    新增选择题

--- src/views/manage/question/questionManage/EditQuestion.vue
+++ src/views/manage/question/questionManage/EditQuestion.vue
@@ -1,12 +1,164 @@
 <template>
-  <div></div>
+  <div class="edit-main" v-loading="data.saveLoad">
+    <div class="operate">
+      <el-button @click="save()">保存</el-button>
+      <el-button @click="backList()">返回</el-button>
+    </div>
+    <div class="body-question">
+      <div class="head-type">
+        <div class="label">题型</div>
+        <el-select v-model="data.question.type" style="margin-right:40px">
+          <el-option
+            v-for="item in data.types"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+        <div class="label">难度</div>
+        <el-select v-model="data.question.hard">
+          <el-option
+            v-for="item in data.hards"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </div>
+      <div class="topic">
+        <div class="label">题目</div>
+        <el-input v-model="data.question.topic" :rows="4" type="textarea"></el-input>
+      </div>
+      <div class="content">
+        <section class="option">
+          <div class="label">答案</div>
+          <el-select v-model="data.answer">
+            <el-option
+              v-for="item in data.options"
+              :key="item.option"
+              :value="item.option"
+              :label="`选项${item.option}`"
+            ></el-option>
+          </el-select>
+        </section>
+        <section v-for="item in data.options" :key="item.option" class="option">
+          <div class="label">选项{{ item.option }}:</div>
+          <el-input v-model="item.value" :rows="2" type="textarea" placeholder="请填写选项内容"></el-input>
+        </section>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ElMessage } from 'element-plus'
 import { reactive, onMounted } from 'vue'
-const data = reactive({})
+import { post } from '../../../../http/request'
+const props = defineProps({
+  control: Boolean,
+  editType: String,
+  data: Object,
+})
+const emit = defineEmits(['update:control'])
+const data = reactive({
+  question: {
+    type: 0,
+    topic: '',
+    hard: 0,
+    options: '',
+    answer: '',
+  },
+  types: [
+    { value: 0, label: '单选题' },
+    { value: 1, label: '多选题' },
+    { value: 2, label: '判断题' },
+    { value: 3, label: '填空题' },
+  ],
+  hards: [
+    { value: 0, label: '简单' },
+    { value: 1, label: '中等' },
+    { value: 2, label: '困难' },
+  ],
+  options: [
+    { option: 'A', value: '' },
+    { option: 'B', value: '' },
+    { option: 'C', value: '' },
+    { option: 'D', value: '' }
+  ],
+  answer: 'A',
+  saveLoad: false
+})
+
+const addQuestion = async () => {
+  data.saveLoad = true;
+  let res = await post('/question/add', data.question);
+  data.saveLoad = false;
+  if (res.status === 1000) {
+    ElMessage.success('保存成功');
+    backList();
+  } else {
+    ElMessage.error(res.desc);
+  }
+}
+
+const backList = () => {
+  emit('update:control', true);
+}
+
+const save = () => {
+  if (data.question.type === 0) {
+    data.question.options = JSON.stringify(data.options);
+    data.question.answer = data.answer;
+  }
+  addQuestion();
+}
 onMounted(() => {
+  if (props.editType === 'edit') {
+    data.question = props.data;
+    data.question.hard = Number(props.data.hard)
+    if (props.data.type === 0) {
+      data.options = JSON.parse(props.data.options);
+      data.answer = props.data.answer;
+    }
+  }
 })
 </script>
 <style scoped lang='less'>
+.main {
+  display: flex;
+  .operate {
+    text-align: right;
+  }
+  .body-question {
+    .head-type {
+      display: flex;
+      align-items: center;
+      margin-bottom: 20px;
+      .label {
+        width: 50px;
+        margin-right: 10px;
+      }
+    }
+    .topic {
+      display: flex;
+      margin-bottom: 20px;
+      .label {
+        min-width: 50px;
+        margin-right: 10px;
+        line-height: 36px;
+      }
+    }
+    .content {
+      .option {
+        display: flex;
+        margin-bottom: 20px;
+        .label {
+          min-width: 50px;
+          margin-right: 10px;
+          line-height: 36px;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/views/manage/question/questionManage/QuestionManage.vue
+++ src/views/manage/question/questionManage/QuestionManage.vue
@@ -1,67 +1,86 @@
 <template>
   <div class="main">
-    <div class="search">
-      <el-input v-model="data.params.param.keyword" placeholder="输入关键词" class="margin-r wit-3"></el-input>
-      <el-button @click="toSearch" :loading="data.searchLoad">搜索</el-button>
-      <el-button @click="addQuestion">添加试题</el-button>
-    </div>
-    <el-table :data="data.tableData" border style="width: 100%" v-loading="data.tableLoad">
-      <el-table-column label="题目" prop="topic" width="200"></el-table-column>
-      <el-table-column label="创建时间" prop="createTime" width="180">
-        <template #default="scope">
-          <span
-            class="table-span"
-            :title="scope.row.createTime"
-          >{{ scope.row.createTime ? scope.row.createTime : '---' }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="最后修改时间" prop="changeTime"></el-table-column>
-      <el-table-column label="最后操作人" prop="changeUserName"></el-table-column>
-      <el-table-column label="操作" width="120">
-        <template #default="scope">
-          <div class="table-operate">
-            <el-button type="text" @click="edit(scope.row)">编辑</el-button>
-            <el-divider direction="vertical"></el-divider>
-            <el-button type="text">删除</el-button>
-          </div>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="pagination">
-      <el-pagination
-        :pager-count="5"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :page-sizes="[1, 4, 8, 10]"
-        background
-        layout="total, prev, pager, next, sizes, jumper"
-        :total="data.total"
-      ></el-pagination>
-    </div>
+    <transition name="el-zoom-in-center">
+      <div v-if="data.showList">
+        <div class="search">
+          <el-input v-model="data.params.param.keyword" placeholder="输入关键词" class="margin-r wit-3"></el-input>
+          <el-button @click="toSearch" :loading="data.searchLoad">搜索</el-button>
+          <el-button @click="addQuestion">添加试题</el-button>
+        </div>
+        <el-table :data="data.tableData" border style="width: 100%" v-loading="data.tableLoad">
+          <el-table-column label="题目" prop="topic"></el-table-column>
+          <el-table-column label="创建时间" prop="createTime" width="180">
+            <template #default="scope">
+              <span
+                class="table-span"
+                :title="scope.row.createTime"
+              >{{ scope.row.createTime ? scope.row.createTime : '---' }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="最后修改时间" prop="changeTime" width="180"></el-table-column>
+          <el-table-column label="最后操作人" prop="changeUser" width="150"></el-table-column>
+          <el-table-column label="操作" width="120">
+            <template #default="scope">
+              <div class="table-operate">
+                <el-button type="text" @click="edit(scope.row)">编辑</el-button>
+                <el-divider direction="vertical"></el-divider>
+                <el-button type="text">删除</el-button>
+              </div>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="pagination">
+          <el-pagination
+            :pager-count="5"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :page-sizes="[1, 4, 8, 10]"
+            background
+            layout="total, prev, pager, next, sizes, jumper"
+            :total="data.total"
+          ></el-pagination>
+        </div>
+      </div>
+    </transition>
+    <transition name="el-zoom-in-center">
+      <EditQuestion
+        v-if="!data.showList"
+        :editType="data.editType"
+        :data="data.editQuestion"
+        v-model:control="data.showList"
+      ></EditQuestion>
+    </transition>
   </div>
 </template>
 
 <script setup>
-import { reactive, onMounted } from 'vue'
+import { reactive, onMounted, watch } from 'vue'
 import { post } from '../../../../http/request';
+import EditQuestion from './EditQuestion.vue';
 const data = reactive({
   params: {
     pageNumber: 1,
     pageSize: 10,
     param: {
-      keyword:'',
-      type:'',
-      hard:''
+      keyword: '',
+      type: '',
+      hard: ''
     }
   },
   total: 0,
   tableData: [],
   searchLoad: false,
   tableLoad: false,
-  editType:'add',
-  editQuestion:{},
+  editType: 'add',
+  editQuestion: {},
+  showList: true
 })
-
+watch(() => data.showList,
+  (n, o) => {
+    if (n) {
+      toSearch();
+    }
+  })
 const getQuestionList = async () => {
   data.tableLoad = true;
   let res = await post('/question/page', data.params);
@@ -91,7 +110,14 @@ const handleSizeChange = val => {
 }
 
 const addQuestion = () => {
+  data.editType = 'add';
+  data.showList = false;
+}
 
+const edit = row => {
+  data.editQuestion = JSON.parse(JSON.stringify(row));
+  data.editType = 'edit';
+  data.showList = false;
 }
 
 onMounted(() => {
